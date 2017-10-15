ac4db3f55eac24bd4646432dda9f5540159c43d9
Benjamin Wilkins <wilkben@yahoo.ca>
2017-10-15 17:33:36 -0400

    added simple moving average filter

--- Anemometer_Filters/Anemometer_Filters.ino
+++ Anemometer_Filters/Anemometer_Filters.ino
@@ -1,5 +1,5 @@
 // Constants for exponential filter
-float expWeightingFactor = 0.07;
+const float expWeightingFactor = 0.07;
 
 int expValue = 0;
 bool expFirstValueFlag = true;
@@ -21,6 +21,59 @@ int exponentialFilter(int reading){
     return (expValue + 360) % 360;
 }
 
+
+// Constants for average filter
+const int averageFactor = 30;
+
+int avgValue[averageFactor+1];
+int lastAvg = 0;
+bool avgFirstValueFlag = true;
+int avgWrapValue = 0;
+int averageFilter(int reading){
+    //first value
+    if (avgFirstValueFlag == true){
+        for (int i = 0; i <= averageFactor; i++){
+            avgValue[i] = reading;
+        }
+        lastAvg = reading;
+        avgFirstValueFlag = false;
+    }else{
+        //shift values
+        for (int i = averageFactor; i > 0; i--){
+            avgValue[i] = avgValue[i-1];
+        }
+        avgValue[0] = reading;
+    }
+
+    //deal with wrapping
+    if (reading < 90 && lastAvg > 270){
+        lastAvg -= 360;
+    }else if (reading > 270 && lastAvg < 90){
+        lastAvg += 360;
+    }
+
+    //keep track of whether reading has wrapped further than tail of average
+    if (reading < 90 && avgValue[1] > 270){
+        avgWrapValue -= 1;
+    }else if (reading > 270 && avgValue[1] < 90){
+        avgWrapValue += 1;
+    }
+
+    //calculate average
+    lastAvg += reading/averageFactor - (avgValue[averageFactor]+360*avgWrapValue)/averageFactor;
+    lastAvg = (lastAvg + 360) % 360;
+
+    //track tail wrap
+    if (avgValue[averageFactor-1] < 90 && avgValue[averageFactor] > 270){
+        avgWrapValue += 1;
+    }else if (avgValue[averageFactor-1] > 270 && avgValue[averageFactor] < 90){
+        avgWrapValue -= 1;
+    }
+
+    return lastAvg;
+}
+
+
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
@@ -34,8 +87,8 @@ void loop() {
     // put your main code here, to run repeatedly:
     int reading = getData();
     
-    Serial.println(exponentialFilter(reading));
-    delay(100);
+    Serial.println(averageFilter(reading));
+    delay(10);
     x+=1;
     
     if (x == 3000){
