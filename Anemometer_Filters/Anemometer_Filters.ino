4ef760d2e8f1d5247081075041706cacdee316f0
Benjamin Wilkins <wilkben@yahoo.ca>
2017-10-15 13:53:36 -0400

    increased indentation

--- Anemometer_Filters/Anemometer_Filters.ino
+++ Anemometer_Filters/Anemometer_Filters.ino
@@ -4,42 +4,42 @@ float expWeightingFactor = 0.07;
 int expValue = 0;
 bool expFirstValueFlag = true;
 int exponentialFilter(int reading){
-  //first value
-  if (expFirstValueFlag == true){
-    expValue = reading;
-    expFirstValueFlag = false;
-  }else{
-    //deal with vaue wrapping
-    if (reading < 90 && expValue > 270){
-      expValue -= 360;
-    }else if (reading > 270 && expValue < 90){
-      expValue += 360;
-    }
+    //first value
+    if (expFirstValueFlag == true){
+        expValue = reading;
+        expFirstValueFlag = false;
+    }else{
+        //deal with vaue wrapping
+        if (reading < 90 && expValue > 270){
+            expValue -= 360;
+        }else if (reading > 270 && expValue < 90){
+            expValue += 360;
+        }
     expValue = expWeightingFactor*reading + (1 - expWeightingFactor)*expValue;
-  }
-  
-  return (expValue + 360) % 360;
+    }
+    
+    return (expValue + 360) % 360;
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Start");
-  //So we'll have the same data each time
-  randomSeed(4);
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.println("Start");
+    //So we'll have the same data each time
+    randomSeed(4);
 }
 
 int x = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  int reading = getData();
-
-  Serial.println(exponentialFilter(reading));
-  delay(100);
-  x+=1;
-
-  if (x == 3000){
-    Serial.println("5 minutes");
-  }
+    // put your main code here, to run repeatedly:
+    int reading = getData();
+    
+    Serial.println(exponentialFilter(reading));
+    delay(100);
+    x+=1;
+    
+    if (x == 3000){
+        Serial.println("5 minutes");
+    }
 }
 
--- Anemometer_Filters/Simulated_Data.ino
+++ Anemometer_Filters/Simulated_Data.ino
@@ -17,79 +17,79 @@ const float spikeVelDecrease = 3; // Rate at which spike slowd
 const float spikeDecrease = 15; // Rate at which spike goes back down
 
 float sign(float value) { 
- return float((value>0)-(value<0)); 
+return float((value>0)-(value<0)); 
 }
 
 float smoothAccel = 0, smoothVel = 0, smooth = 180;
 int smoothData() {
-  if (smoothAccel == 0){
-    //random chance of sudden larger change
-    if (random(0,accelChance) == 0 && smoothVel < maxVel && smoothVel > -1 * maxVel){
-      smoothAccel = random(-100, 100)*(accelRate/100);
+    if (smoothAccel == 0){
+        //random chance of sudden larger change
+        if (random(0,accelChance) == 0 && smoothVel < maxVel && smoothVel > -1 * maxVel){
+            smoothAccel = random(-100, 100)*(accelRate/100);
+        }
+        //small random changes
+        if (smoothVel < maxVel && smoothVel > -1 * maxVel){
+            smoothVel += random(-100, 100)*(velChange/100);
+        }else{ //slow down after larger change
+            smoothVel -= sign(smoothVel) * random(0, 100)*(velDecrease/100);
+        }
+    }else{
+        //slow down after larger change
+        smoothAccel -= sign(smoothAccel) * random(0, 100)*(accelDecrease/100);
+        if (smoothAccel < accelDecrease && smoothAccel > -1 * accelDecrease){
+            smoothAccel = 0;
+        }
     }
-    //small random changes
-    if (smoothVel < maxVel && smoothVel > -1 * maxVel){
-      smoothVel += random(-100, 100)*(velChange/100);
-    }else{ //slow down after larger change
-      smoothVel -= sign(smoothVel) * random(0, 100)*(velDecrease/100);
-    }
-  }else{
-    //slow down after larger change
-    smoothAccel -= sign(smoothAccel) * random(0, 100)*(accelDecrease/100);
-    if (smoothAccel < accelDecrease && smoothAccel > -1 * accelDecrease){
-      smoothAccel = 0;
-    }
-  }
 
-  smoothVel += smoothAccel;
-  smooth += smoothVel;
-
-  return (int) smooth; 
+    smoothVel += smoothAccel;
+    smooth += smoothVel;
+    
+    return (int) smooth; 
 }
 
 float noiseLevel = 0;
 int noise(){
-  if (noiseLevel > maxNoise){
-    noiseLevel -= 1;
-  }else if (noiseLevel < (-1) * maxNoise){
-    noiseLevel += 1;
-  }else{
-    noiseLevel += random(-100, 100) * (noiseChange/100);
-  }
-
-  return (int) noiseLevel;
+    if (noiseLevel > maxNoise){
+        noiseLevel -= 1;
+    }else if (noiseLevel < (-1) * maxNoise){
+        noiseLevel += 1;
+    }else{
+        noiseLevel += random(-100, 100) * (noiseChange/100);
+    }
+    
+    return (int) noiseLevel;
 }
 
 float spikeLevel = 0, spikeVel = 0;
 int spike(){
-  if (spikeLevel == 0){
-    // Randomly cause spikes
-    if (random(0, spikeChance) == 0){
-      spikeVel = random(-100,100) * (spikeMaxVel/100);
-    }
-  }else{
-    if (spikeVel == 0){
-      //Bring spike back down
-      spikeLevel -= sign(spikeLevel) * random(0, 100) * (spikeDecrease / 100);
-    }else if (spikeVel < spikeVelDecrease && spikeVel > (-1) * spikeVelDecrease){
-      spikeVel = 0;
+    if (spikeLevel == 0){
+        // Randomly cause spikes
+        if (random(0, spikeChance) == 0){
+            spikeVel = random(-100,100) * (spikeMaxVel/100);
+        }
     }else{
-      // Slow down spike
-      spikeVel -= sign(spikeVel) * random(0, 100) * (spikeVelDecrease / 100);
+        if (spikeVel == 0){
+            //Bring spike back down
+            spikeLevel -= sign(spikeLevel) * random(0, 100) * (spikeDecrease / 100);
+        }else if (spikeVel < spikeVelDecrease && spikeVel > (-1) * spikeVelDecrease){
+            spikeVel = 0;
+        }else{
+            // Slow down spike
+            spikeVel -= sign(spikeVel) * random(0, 100) * (spikeVelDecrease / 100);
+        }
+        
+        //Prevent oscillation at end of spike
+        if (spikeLevel < spikeDecrease && spikeLevel > -1 * spikeDecrease){
+            spikeLevel = 0;
+        }
     }
     
-    //Prevent oscillation at end of spike
-    if (spikeLevel < spikeDecrease && spikeLevel > -1 * spikeDecrease){
-      spikeLevel = 0;
-    }
-  }
-  
-  spikeLevel += spikeVel;
-
-  return spikeLevel;
+    spikeLevel += spikeVel;
+    
+    return spikeLevel;
 }
 
 int getData(){
-  return ((((smoothData() + noise() + spike()) % 360) + 360) % 360); // because mod with negatives is dumb
+    return ((((smoothData() + noise() + spike()) % 360) + 360) % 360); // because mod with negatives is dumb
 }
 
